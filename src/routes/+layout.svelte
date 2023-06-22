853e39ce558faa2b1e7c13b2429aba8b2c872edc
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-06-22 17:28:19 +0100

    feat: complete homepage mobile view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
+	import Footer from '../components/molecules/Footer.svelte';
 	import Navbar from '../components/molecules/Navbar.svelte';
 </script>
 
 <main class="relative">
 	<Navbar />
 	<slot />
+	<Footer />
 </main>
