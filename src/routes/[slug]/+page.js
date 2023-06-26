9bacb3ea080568cd90a30b3a3e098e6b3d6ffc71
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-06-26 08:32:08 +0100

    feat: add all products category page

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -1,12 +1,6 @@
 import data from '../../data/data.json';
 
 export function load({ params }) {
-	const getCategory = () => {
-		const category = data.filter((curr) => curr.category == params.slug);
-
-		return category;
-	};
-	return {
-		products: getCategory()
-	};
+	const products = data.filter((curr) => curr.category === params.slug);
+	return { products };
 }
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,25 +1,31 @@
 <script>
 	import Button from '../../components/atoms/Button.svelte';
 	export let data;
-	const { products } = data;
-	console.log(products[0].categoryImage);
+
+	$: products = data.products;
 </script>
 
-<section class="max-w-5xl mx-auto">
-	{#each products as product}
-		<section class="px-6 py-8 md:px-10 lg:px-0 flex flex-col items-center gap-6">
-			<div class="rounded-2xl">
+<section class="max-w-5xl mx-auto flex flex-col gap-10 lg:my-16">
+	{#each products as product, id (product.id)}
+		<section
+			class={`px-6 py-8 md:px-10 lg:px-0 flex flex-col items-center gap-10  ${
+				id % 2 == 1 ? 'lg:flex-row-reverse' : 'lg:flex-row'
+			} lg:justify-between`}
+		>
+			<div class="rounded-2xl overflow-hidden lg:w-1/2">
 				<picture>
-					<source srcset={product.categoryImage.desktop} media="(mid-width: 1024px)" />
-					<source srcset={product.categoryImage.tablet} media="(mid-width: 768px)" />
+					<source srcset={product.categoryImage.desktop} media="(min-width: 1024px)" />
+					<source srcset={product.categoryImage.tablet} media="(min-width: 768px)" />
 					<img src={product.categoryImage.mobile} alt={`image of a ${product.name}`} />
 				</picture>
 			</div>
-			<div class="text-center flex flex-col items-center gap-4">
+			<div
+				class="text-center flex flex-col items-center gap-4 md:w-2/3 lg:w-1/2 lg:items-start lg:text-start lg:px-8"
+			>
 				{#if product.new}
 					<p class="uppercase text-browns tracking-[.5rem] text-sm">new product</p>
 				{/if}
-				<h1 class="uppercase font-semibold text-3xl md:text-4xl">{product.name}</h1>
+				<h1 class="uppercase font-semibold text-3xl md:text-4xl lg:w-2/3">{product.name}</h1>
 				<p>{product.description}</p>
 				<Button bgcolor="browns" textcolor="white" fn={null} border="browns">see product</Button>
 			</div>
