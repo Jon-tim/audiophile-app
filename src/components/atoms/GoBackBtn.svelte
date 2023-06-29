30a9216fa4feff684d39bd86d28144064218a230
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-06-29 15:30:56 +0100

    Refactor: Structure o-back-to-previous-page button

---
+++ src/components/atoms/GoBackBtn.svelte
@@ -0,0 +1,10 @@
+<button
+	class="px-4 py-2 bg-black/5 rounded-md w-max cursor-pointer group mb-8"
+	on:click={() => window.history.back()}
+>
+	<p
+		class="font-semibold capitalize group-hover:text-browns transition-colors duration-200 ease-linear"
+	>
+		go back
+	</p>
+</button>
